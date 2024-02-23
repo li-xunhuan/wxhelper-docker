171da80f8e0a07bd2345bc7db679fccefff48f89
李寻欢 <lxh@cxh.cn>
2024-02-23 10:48:57 +0800

    完善脚本，支持32位和64位微信自动启动

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ ARG WXHELPER_URL=https://github.com/ttttupup/wxhelper/releases/download/3.9.5.81
 
 WORKDIR /home/app/.wine/drive_c
 
+# 补充Windows字体
+COPY ./Fonts ./windows/Fonts
+
 # 加载注入器
 ADD https://github.com/furacas/DllInjector/releases/download/v1.4.0/DllInjector64.exe DllInjector.exe
 RUN sudo chown app:app DllInjector.exe && sudo chmod a+x DllInjector.exe
--- cmd.sh
+++ cmd.sh
@@ -3,7 +3,13 @@
 exec sudo -E bash -c 'supervisord -c /etc/supervisord.conf -l /var/log/supervisord.log' &
 sleep 10
 
-wine 'C:\Program Files\Tencent\WeChat\WeChat.exe' &
+if [ ! -d "/home/app/.wine/drive_c/Program Files/Tencent" ]; then
+  echo '启动64位微信'
+  wine 'C:\Program Files\Tencent\WeChat\WeChat.exe' &
+else
+  echo '启动32位微信'
+  wine 'C:\Program Files (x86)\Tencent\WeChat\WeChat.exe' &
+fi
 
 sleep 10
 
